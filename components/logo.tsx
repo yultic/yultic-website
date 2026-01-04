e6bd071cc8907ff0244e466b602e3f857cafde50
Chinaskidev <chinaskicharles0@gmail.com>
2026-01-03 22:44:57 -0300

    cambiando logo

--- components/logo.tsx
+++ components/logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 export function Logo() {
   return (
     <div className="flex items-center gap-2 group cursor-pointer">
-      <Image src="/yultic.png" alt="Yultic Logo" width={80} height={80} />
+      <Image src="/yultic_letras3.png" alt="Yultic Logo" width={130} height={80} />
     </div>
   )
 }
